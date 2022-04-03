2f2cb1878870d0e0a29f039dde526f259876d3c9
Justin Selsor <jdselsor@gmail.com>
2022-04-02 17:44:16 -0700

    inital commit

---
+++ src/include/ecs.h
@@ -0,0 +1,41 @@
+#ifndef ECS_H
+#define ECS_H
+
+#include "raylib/raylib.h"
+
+typedef struct {
+    Vector2 position;
+    Vector2 dimensions;
+} TransformComponent;
+
+typedef struct {
+    bool isStatic;
+    float gravity;
+} PhysicsComponent;
+
+typedef struct {
+    Color color;
+} RenderBoxComponent;
+
+typedef struct {
+    TransformComponent *transformComponent;
+    PhysicsComponent *physicsComponent;
+    RenderBoxComponent *renderBoxComponent;
+} Components;
+
+typedef struct {
+    unsigned int uuid;
+    Components components;
+} Entity;
+
+void add_transform_component (Entity *e, Vector2 position, Vector2 dimensions);
+void add_physics_component (Entity *e, bool isStatic, float gravity);
+void add_render_box_component (Entity *e, Color color);
+
+void free_entity (Entity *e);
+
+// Component Logic
+void render_box (Entity *e);
+bool check_collision (Entity *e1, Entity *e2);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include "raylib/raylib.h"
+#include "ecs.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+
+int main () {
+    InitWindow (SCREEN_WIDTH, SCREEN_HEIGHT, "LD50 Game");
+    SetTargetFPS (60);
+
+    Entity rect;
+    add_transform_component(&rect, (Vector2) {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2}, (Vector2) {20, 20});
+    add_render_box_component(&rect, LIGHTGRAY);
+
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        
+        render_box(&rect);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
